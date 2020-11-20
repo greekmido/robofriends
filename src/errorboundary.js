b96044bb676c4194d81a476f1ed200eac0b926c5
greekmido <onex911@hotmail.com>
2020-11-20 08:54:46 +0000

    new feature

---
+++ src/errorboundary.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Error extends Component{
+    constructor(props){
+        super(props);
+       this.state={
+           hasError:false
+       }
+    }
+}
+
+export default Error
